a71dbb002c92216d56a155013175e6cc8ebee7bc
Olgun Erguzel <olgunerguzel@Olguns-MBP.fritz.box>
2021-01-31 02:53:37 +0100

    prepare script list separate package installation

---
+++ preapre.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+
+python3 -m venv env
+
+source env/bin/activate
+
+pip3 install --upgrade pip
+
+pip3 install qiskit --no-cache-dir
+pip3 install jupyter --no-cache-dir
+
+## matplotlib
+pip3 install matplotlib --no-cache-dir
+## matplotlib mpl output
+pip3 install pylatexenc --no-cache-dir
+## qiskit visualisations for mac in quotes
+pip3 install 'qiskit[visualization]'
+
